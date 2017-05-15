14e866ae6f3b8fac453b657999ccd65fab6c21bb
sguzunov <sguzunov@gmail.com>
2017-05-15 17:17:33 +0300

    Added a WPF client.

---
+++ PingPong/PingPong.UI.Wpf/WpfRenderer.cs
@@ -0,0 +1,35 @@
+﻿using PingPong.Logic.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PingPong.Logic.GameObjects.Contracts;
+using System.Windows.Controls;
+
+namespace PingPong.UI.Wpf
+{
+    public class WpfRenderer : IRenderer
+    {
+        private readonly Canvas context;
+
+        public WpfRenderer(Canvas context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public double FieldWidth => this.context.ActualWidth;
+
+        public double FieldHeight => this.context.ActualHeight;
+
+        public void DrawBall(IBall ball)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawPlayers(IEnumerable<IPlayer> players)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
